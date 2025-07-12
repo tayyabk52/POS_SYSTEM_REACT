f380e82ee3f2d965904ab91dead9c4135b8d5f28
abdullah-a8 <168286028+abdullah-a8@users.noreply.github.com>
2025-07-13 02:53:41 +0500

    Enhance README with database setup instructions and update database connection handling in backend to use environment variables from .env file.

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 import os
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+# Load environment variables from .env file
+load_dotenv()
 
 DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_PORT = os.getenv('DB_PORT', '5432')
@@ -9,7 +14,11 @@ DB_NAME = os.getenv('DB_NAME', 'POSSYSTEM')
 DB_USER = os.getenv('DB_USER', 'postgres')
 DB_PASS = os.getenv('DB_PASS', 'admin')
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print("DB_USER:", repr(DB_USER))
+
+DB_PASS_ENCODED = quote_plus(DB_PASS)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS_ENCODED}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
